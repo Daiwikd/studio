24fd59ff7df0530dd57aab27481a21bcc97b8cf3
Anindita Das <dassanindita789@gmail.com>
2025-11-17 15:47:00 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3011907721-de151",
+  "appId": "1:997359731512:web:12925df2f54f2dc57877cd",
+  "apiKey": "AIzaSyC_BWIDlWZLta2OqQJIWaFAa8_Z3WQEMAY",
+  "authDomain": "studio-3011907721-de151.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "997359731512"
+};

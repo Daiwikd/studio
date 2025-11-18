bfd43221fc6083f323cbef310164368bda387ce6
Anindita Das <dassanindita789@gmail.com>
2025-11-18 10:20:19 +0000

    Double the w

--- src/app/quizzes/page.tsx
+++ src/app/quizzes/page.tsx
@@ -64,7 +64,7 @@ function QuizList() {
     return (
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {[...Array(4)].map((_, i) => (
-          <Card key={i} className="flex flex-col justify-center items-center h-40 w-40">
+          <Card key={i} className="flex flex-col justify-center items-center h-40 w-80">
             <CardHeader className="p-2 pt-4">
               <Skeleton className="h-5 w-24 mx-auto" />
               <Skeleton className="h-4 w-16 mx-auto" />
@@ -98,7 +98,7 @@ function QuizList() {
     <AlertDialog>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
         {quizzes.map((quiz) => (
-          <Card key={quiz.id} className="flex flex-col justify-center items-center text-center h-40 w-40">
+          <Card key={quiz.id} className="flex flex-col justify-center items-center text-center h-40 w-80">
             <CardHeader className="p-2 pt-4">
               <CardTitle className="font-headline text-base">{quiz.title}</CardTitle>
               <CardDescription>{quiz.questions.length} questions</CardDescription>
